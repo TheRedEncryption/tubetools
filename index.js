86f4d952ca7144cec8986b0e8d56c7687b68c151
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2023-10-26 17:00:11 +0000

    made the website look prettier :P

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var isRecording = false;
-
+var recordButton;
 
 document.addEventListener("DOMContentLoaded", (event) => {
-
+    recordButton = document.getElementById("recordButton");
 });
 
 function getPerms(){
     navigator.mediaDevices.getUserMedia({audio:true});
     isRecording = true;
+
+    recordButton.style.setProperty('--inversion', 100);
+    recordButton.style.setProperty('background-color', 'red');
 }

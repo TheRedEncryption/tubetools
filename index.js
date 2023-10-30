ab9c0aa42d0714bb431918d29ed523acce48ff89
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2023-10-30 12:53:43 +0000

    i made the thing work :D :D :D :D :D :D :D :D :D

--- index.js
+++ index.js
@@ -6,9 +6,45 @@ document.addEventListener("DOMContentLoaded", (event) => {
 });
 
 function getPerms(){
-    navigator.mediaDevices.getUserMedia({audio:true});
+    getMicrophoneStream();
     isRecording = true;
 
     recordButton.style.setProperty('--inversion', 100);
     recordButton.style.setProperty('background-color', 'red');
+}
+
+async function getMicrophoneStream() {
+    try{
+        const stream = await navigator.mediaDevices.getUserMedia({audio:true});
+        const audioContext = new AudioContext();
+        const source = audioContext.createMediaStreamSource(stream);
+        const analyser = audioContext.createAnalyser();
+        source.connect(analyser);
+
+        const bufferLength = analyser.frequencyBinCount;
+        const dataArray = new Uint8Array(bufferLength);
+
+        // do something with thing here
+
+        const volumeMeter = document.getElementById("volumeMeter");
+
+        function draw() {
+            requestAnimationFrame(draw);
+
+            analyser.getByteTimeDomainData(dataArray);
+
+            let sum = 0;
+            for(let i = 0; i < bufferLength; i++){
+                sum += Math.abs(dataArray[i] - 128);
+            }
+            const volume = sum / bufferLength;
+
+            volumeMeter.style.height = volume * 3 + '%';
+        }
+
+        draw();
+    }
+    catch(error){
+        alert(error);
+    }
 }
